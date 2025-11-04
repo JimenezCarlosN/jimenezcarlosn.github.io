a0d3a9d8ce9af551fd8f8a18e7ab8f8b54b2f280
JimenezCarlosN <cnjimenez@campus.ungs.edu.ar>
2025-11-03 21:56:47 -0300

    Add files via upload

---
+++ Proyecto C/ProyectoUnity.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Se ejecuta una vez al iniciar la escena
+    void Start()
+    {
+        Debug.Log("¡Hola Mundo desde Unity!");
+    }
+}
